ad3bf246587461da8c98aae8c465edc37b447b7a
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2023-12-17 12:20:15 +0300

    Update Main.java

--- Mayın Tarlası Oyunum/Main.java
+++ Mayın Tarlası Oyunum/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MineSweeper game = new MineSweeper(5,5);
+        MineSweeper game = new MineSweeper(5,5);//oyun tahtasının boyutunu kendinize gore belirleyebilirsiniz
         System.out.println("---> Oyunuma Hosgeldiniz <---");
         game.run();
     }
