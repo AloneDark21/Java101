c69bfb5dc20a2c3a75d00f22fe8af609963f2f03
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2023-12-18 00:31:16 +0300

    Create Main.java

---
+++ Selection Sort ve Insertion Sort/Selection Sort/Main.java
@@ -0,0 +1,10 @@
+package SelectionShortAlgoritmasi;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {7,3,5,8,2,9,4,15,6};
+
+        SelectionSort selectionSort = new SelectionSort(arr);
+    }
+}
