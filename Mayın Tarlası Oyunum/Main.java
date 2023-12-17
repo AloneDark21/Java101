60588e4af0e8d27668981fea1c63112f0beb55ce
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2023-12-17 12:06:16 +0300

    Create Main.java

---
+++ Mayın Tarlası Oyunum/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MineSweeper game = new MineSweeper(5,5);
+        System.out.println("---> Oyunuma Hosgeldiniz <---");
+        game.run();
+    }
+}
