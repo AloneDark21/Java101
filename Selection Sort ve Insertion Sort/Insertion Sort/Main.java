0efb4e75d861c0d7c0822c0a0872154ef7aacf1a
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2023-12-18 00:50:42 +0300

    Create Main.java

---
+++ Selection Sort ve Insertion Sort/Insertion Sort/Main.java
@@ -0,0 +1,11 @@
+package InsertionSort;
+
+public class Main 
+{
+    public static void main(String[] args) {
+        int[] arr = {22,27,16,2,18,6};
+
+        InsertionSort insertionSort = new InsertionSort(arr);
+    }
+    
+}
