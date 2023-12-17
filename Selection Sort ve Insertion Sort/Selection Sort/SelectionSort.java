6e6cd0232a87d7ef0f587f29faa6e59a879b7d6b
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2023-12-18 00:55:14 +0300

    Update SelectionSort.java

--- Selection Sort ve Insertion Sort/Selection Sort/SelectionSort.java
+++ Selection Sort ve Insertion Sort/Selection Sort/SelectionSort.java
@@ -1,3 +1,14 @@
+/*
+[22], 27, 16, 2, 18, 6
+[22, 27], 16, 2, 18, 6
+[22, 27, 16], 2, 18, 6
+[16, 22, 27, 2], 18, 6
+[2, 16, 22, 27, 18], 6
+[2, 16, 18, 22, 27, 6]
+[2, 6, 16, 18, 22, 27]
+
+adımlar şu şekildedir
+*/
 package SelectionShortAlgoritmasi;
 //Denildiği üzere dizinin ilk 4 adımı yazıldı
 public class SelectionSort 
